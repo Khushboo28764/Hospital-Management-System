60b667d8a78bfa1474af2918f7ace0d4a48723e3
Khushboo <khushbookri6782@gmail.com>
2024-08-10 09:20:14 +0530

    first commit

---
+++ fronted/src/components/Biography.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Biography = ({imageUrl}) => {
+  return (
+    <>
+      <div className="container biography">
+        <div className="banner">
+          <img src={imageUrl} alt="whoweare" />
+        </div>
+        <div className="banner">
+          <p>Biography</p>
+          <h3>Who We Are</h3>
+          <p>Doctors diagnose disease, provide treatment, counsel patients with injuries, diseases or illnesses. The specific duties depend upon the speciality you pursue in your MBBS. Some doctors work in cardiology, whereas others may work in surgery, neurology, pulmonology or rheumatology.
+          </p>
+          <p>We are all in 2024!</p>
+          <p>We are working on a MERN STACK PROJECT.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            assumenda exercitationem accusamus sit repellendus quo optio dolorum
+            corporis corrupti. Quas similique vel minima veniam tenetur
+            obcaecati atque magni suscipit laboriosam! Veniam vitae minus nihil
+            cupiditate natus provident. Ex illum quasi pariatur odit nisi
+            voluptas illo qui ipsum mollitia. Libero, assumenda?
+          </p>
+          <p>Lorem ipsum dolor sit amet!</p>
+          <p>Coding is fun!</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Biography;
